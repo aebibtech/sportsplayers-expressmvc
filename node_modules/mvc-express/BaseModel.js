86705c096e9d2798da4bcba5766712b0a52fc1be
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-03-14 21:40:59 +0800

    Add insert to BaseModel class

--- node_modules/mvc-express/BaseModel.js
+++ node_modules/mvc-express/BaseModel.js
@@ -4,25 +4,33 @@ class BaseModel{
         const mysql = require('mysql2');
         this.connection = mysql.createConnection(config.MYSQL_CONFIG);
     }
+    escape(value){
+        return this.connection.escape(value);
+    }
     /** Basic Execute Query function **/
-    query(query) {
+    query(query, parameters = null) {
         const connLoc = this.connection;
         return new Promise(function(resolve, reject){
-            connLoc.query(query, function (err, result) {
+            function queryHandler(err, result) {
                 if(err){
                     reject(err);
                 }else{
                     resolve(result);
                 }
-            });
+            }
+            connLoc.query(query, parameters, queryHandler);
         });	
     }
-    async fetch_all(query){
-        return await this.query(query);
+    async fetch_all(query, parameters = null){
+        return await this.query(query, parameters);
     }
-    async fetch_record(query){
-        const result = await this.query(query);
+    async fetch_record(query, parameters = null){
+        const result = await this.query(query, parameters);
         return result ? result[0] : null;
+    }
+    async insert(query, parameters){
+        const insertData = await this.query(query, parameters);
+        return insertData.insertId;
     }
 }
 
