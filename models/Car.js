f76a6d54fe648d6c9a5a99de1ab6d529b3e196a5
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-03-15 12:06:36 +0800

    Removed sample files

--- models/Car.js
+++
@@ -1,9 +0,0 @@
-const BaseModel = require('../system/BaseModel');
-
-class Car extends BaseModel{
-    getAllCars(){
-        return this.fetch_all('SELECT * FROM cars');
-    }
-}
-
-module.exports = new Car;
